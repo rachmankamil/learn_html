5c7d04df981ed4df7454201683308e4cdab58375
Rachman Kamil <kamil@alterra.id>
2022-06-28 11:28:41 +0700

    node

---
+++ main.js
@@ -0,0 +1,63 @@
+console.log("Hello World, I love programming")
+console.log("My name is Kamil")
+// console.log("My name is Kamil")
+
+let message = "hello"
+let stringNew = "My name is Kamil".length
+console.log(message+stringNew)
+
+let angka = 89.9
+let max = +Infinity
+
+console.log(max, message)
+console.log(message/angka)
+
+console.log(angka<max);
+
+console.log("Before:",messageVar)
+
+const messageConst = "Immersive FE7"
+let messageLet = "Mentor Kamil"
+var messageVar = "Mentenya Banyak"
+
+messageLet = "Mentor Yoga"
+console.log("After:", messageVar);
+
+var messageVar = 1234
+
+console.log("After 2:", messageVar)
+
+const tahunLahir = 1992 // 2022-1992 = 30
+let umur = new Date().getFullYear() - tahunLahir
+let tinggi = 175
+let beratbadan = 88
+// let BMIIndex = "Overweight"
+
+console.log("mod:", 7%2);
+
+umur += 2
+
+console.log("umur:",umur);
+
+// 1 AND 1 = 1 &&
+// 1 AND 0 = 0
+// 0 AND 1 = 0
+// 0 AND 0 = 0
+
+// 1 OR 1 = 1 ||
+// 1 OR 0 = 1
+// 0 OR 1 = 1
+// 0 OR 0 = 0
+
+console.log(9 >>> 1);
+
+// 16  8   4   2   1
+// 1   0   1   0   0 = 20
+// 0   1   0   1   0 = 10
+// 0   0   1   0   1 = 5
+// 0   0   0   1   0 = 2
+// 1   1   0   0   0 = 24
+// 0   1   1   0   0 = 12 
+
+// 1   0   0    1   0 = 18
+// 
---
+++ object.js
@@ -0,0 +1,102 @@
+let obj = {};
+let objConst = new Object();
+objConst.name = "Kamil"
+objConst.age = 29
+objConst.address = "Malang"
+objConst.height = 175
+objConst.college = {
+    name: "SMKN 4 Malang"
+}
+
+let person = objConst
+person.name2 = "Argian"
+
+objConst.address = "Surabaya"
+// let name = "Kamil"
+
+// destructurisasi object
+let {name:fName,address,college:{name:collegeName}} = objConst
+
+// rest syntax (parameter)
+let {name, ...person2} = person
+person2.name = "Vian"
+
+age = 50
+// realObject.college.name = "SMAN 1 Malang"
+
+// console.log(fName, realObject);
+console.log("person:", person);
+console.log("person2:", person2);
+console.log("objConst:", objConst);
+
+let confero = {
+    merk: "Wuling",
+    penggerak: "Belakang",
+    tangiBensin: 45,
+    cruisecontrol: false,
+    merk: "Toyota",
+    mesin: {}
+}
+
+let almas = {
+    merk: "Wuling",
+    penggerak: "Depan",
+    tangiBensin: 60,
+    cruisecontrol: true,
+    mesin: {
+        cc : 1500,
+        turbo : true
+    }
+}
+
+confero.merk = "Wuling"
+confero.topSpeed = 160
+
+confero["penggerak"] = "Depan"
+
+console.log("before:", confero);
+
+// delete properties
+delete confero.topSpeed;
+confero["penggerak"] = ""
+
+console.log("after:", confero);
+
+// jika properties tidak ada, hasilnya undefined
+let valueNotFound = confero["topSpeed"]
+console.log("topSpeed", valueNotFound);
+
+// cek properties value type
+let typeMesin = typeof confero.mesin
+console.log("tipeMesin", typeMesin);
+
+console.log("keys : ", Object.keys(confero));
+console.log("value : ", Object.values(confero));
+
+//clone / copy
+let formo = Object.assign({name:"formo"}, almas, person) 
+// another way - spread syntax
+let person2 = {...person, ...almas}
+
+formo.mesin.turbo = false
+formo.mesin.cc = 1300
+formo.penggerak = "Belakang"
+console.log("formo : ", formo);
+
+let wuling = {
+    almas : almas,
+    confero : confero,
+    formo : formo,
+}
+
+wuling["formo"]["tangiBensin"] = 65
+wuling["formo"]["mesin"]["powersteering"] = false
+wuling.confero.mesin.tpms = true
+console.log(wuling);
+
+let nama = "kamil"
+let nama2 = nama
+console.log("nama:", nama);
+console.log("nama2:", nama2);
+nama2 = "wahyu"
+console.log("nama2-change:", nama2);
