fbf481b0af752ddee0bf613bc898a391d5bec620
Rachman Kamil <kamil@alterra.id>
2022-06-29 09:06:29 +0700

    array

---
+++ array.js
@@ -0,0 +1,75 @@
+let array1 = [];
+let array2 = new Array();
+
+let feBatch7 = [
+  "Wahyu",
+  "Farhan",
+  "Evi",
+  "Argian",
+  90,
+  false,
+  { name: "Kamil" },
+  ["Vian", "Ganjar", "Zak"],
+];
+
+console.log(feBatch7[5]);
+
+feBatch7[2] = "Ikram";
+
+feBatch7[6].name = "Rizal";
+feBatch7[6].school = "SMA 4 Malang";
+feBatch7[7][1] = "Mirzam";
+
+// tidak di sarankan, karena akan jadi masalah
+feBatch7[18] = "dayut";
+// yang terbaik
+let lenArr = feBatch7.push("Ghifari");
+
+console.log("length : ", feBatch7.length);
+console.log("value  : ", feBatch7);
+
+console.table(feBatch7);
+
+let element = feBatch7.pop();
+
+console.log(element);
+
+let result = feBatch7.unshift(50, { name: "oke" }, [89, 90, 45]);
+
+console.log(feBatch7);
+
+let baby = feBatch7.shift();
+
+console.log(feBatch7);
+// tidak di sarankan
+delete feBatch7[0];
+
+console.table(feBatch7);
+
+//delete dari index ke berapa, terus berapa data yang di hapus
+feBatch7.splice(10, 10);
+// di tambahkan data di tengah-tengah
+feBatch7.splice(5, 0, "Kamil", "Vian", 90);
+feBatch7.push({ car: "Wuling" });
+
+console.log("length:", feBatch7.length);
+
+console.log(feBatch7);
+
+// same as object, this is represent
+let cpArray = feBatch7;
+
+cpArray[0] = "are you an object?";
+
+console.log(feBatch7);
+
+//copy array
+let cpArray2 = [...feBatch7];
+cpArray2.push("this is a copy");
+
+console.table(cpArray2);
+console.table(feBatch7);
+
+//slice array - motong-motong
+let cpArray3 = cpArray2.slice(10, 12);
+console.table(cpArray3);
--- object.js
+++ object.js
@@ -1,27 +1,31 @@
 let obj = {};
 let objConst = new Object();
-objConst.name = "Kamil"
-objConst.age = 29
-objConst.address = "Malang"
-objConst.height = 175
+objConst.name = "Kamil";
+objConst.age = 29;
+objConst.address = "Malang";
+objConst.height = 175;
 objConst.college = {
-    name: "SMKN 4 Malang"
-}
+  name: "SMKN 4 Malang",
+};
 
-let person = objConst
-person.name2 = "Argian"
+let person = objConst;
+person.name2 = "Argian";
 
-objConst.address = "Surabaya"
+objConst.address = "Surabaya";
 // let name = "Kamil"
 
 // destructurisasi object
-let {name:fName,address,college:{name:collegeName}} = objConst
+let {
+  name: fName,
+  address,
+  college: { name: collegeName },
+} = objConst;
 
 // rest syntax (parameter)
-let {name, ...person2} = person
-person2.name = "Vian"
+let { name, ...person2 } = person;
+person2.name = "Vian";
 
-age = 50
+age = 50;
 // realObject.college.name = "SMAN 1 Malang"
 
 // console.log(fName, realObject);
@@ -30,73 +34,92 @@ console.log("person2:", person2);
 console.log("objConst:", objConst);
 
 let confero = {
-    merk: "Wuling",
-    penggerak: "Belakang",
-    tangiBensin: 45,
-    cruisecontrol: false,
-    merk: "Toyota",
-    mesin: {}
-}
+  merk: "Wuling",
+  penggerak: "Belakang",
+  tangiBensin: 45,
+  cruisecontrol: false,
+  merk: "Toyota",
+  mesin: {},
+};
 
 let almas = {
-    merk: "Wuling",
-    penggerak: "Depan",
-    tangiBensin: 60,
-    cruisecontrol: true,
-    mesin: {
-        cc : 1500,
-        turbo : true
-    }
-}
+  merk: "Wuling",
+  penggerak: "Depan",
+  tangiBensin: 60,
+  cruisecontrol: true,
+  mesin: {
+    cc: 1500,
+    turbo: true,
+  },
+};
 
-confero.merk = "Wuling"
-confero.topSpeed = 160
+confero.merk = "Wuling";
+confero.topSpeed = 160;
 
-confero["penggerak"] = "Depan"
+confero["penggerak"] = "Depan";
 
 console.log("before:", confero);
 
 // delete properties
 delete confero.topSpeed;
-confero["penggerak"] = ""
+confero["penggerak"] = "";
 
 console.log("after:", confero);
 
 // jika properties tidak ada, hasilnya undefined
-let valueNotFound = confero["topSpeed"]
+let valueNotFound = confero["topSpeed"];
 console.log("topSpeed", valueNotFound);
 
 // cek properties value type
-let typeMesin = typeof confero.mesin
+let typeMesin = typeof confero.mesin;
 console.log("tipeMesin", typeMesin);
 
 console.log("keys : ", Object.keys(confero));
 console.log("value : ", Object.values(confero));
 
 //clone / copy
-let formo = Object.assign({name:"formo"}, almas, person) 
+let formo = Object.assign({ name: "formo" }, almas, person);
 // another way - spread syntax
-let person2 = {...person, ...almas}
+let person3 = { ...person, ...almas };
 
-formo.mesin.turbo = false
-formo.mesin.cc = 1300
-formo.penggerak = "Belakang"
+formo.mesin.turbo = false;
+formo.mesin.cc = 1300;
+formo.penggerak = "Belakang";
 console.log("formo : ", formo);
 
 let wuling = {
-    almas : almas,
-    confero : confero,
-    formo : formo,
-}
+  almas: almas,
+  confero: confero,
+  formo: formo,
+  0: "nama",
+  23.5: "text",
+  true: true,
+};
 
-wuling["formo"]["tangiBensin"] = 65
-wuling["formo"]["mesin"]["powersteering"] = false
-wuling.confero.mesin.tpms = true
+wuling["formo"]["tangiBensin"] = 65;
+wuling["formo"]["mesin"]["powersteering"] = false;
+wuling.confero.mesin.tpms = true;
 console.log(wuling);
 
-let nama = "kamil"
-let nama2 = nama
+let nama = "kamil";
+let nama2 = nama;
 console.log("nama:", nama);
 console.log("nama2:", nama2);
-nama2 = "wahyu"
+nama2 = "wahyu";
 console.log("nama2-change:", nama2);
+
+let wulingArr = [almas, formo, confero];
+
+let almas2 = wulingArr[0];
+almas2.merk = "Chevrolet";
+
+wulingArr[0].height = 200;
+
+console.table(wulingArr);
+
+let gender = {
+  0: "laki-laki",
+  1: "perempuan",
+};
+
+console.log(gender);
